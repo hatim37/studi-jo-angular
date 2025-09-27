1e02896505a396670030864c96dba3609781fed7
unknown <hatim.web.dev@gmail.com>
2025-09-27 15:47:03 +0200

    gitignore

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  backend_products : "http://localhost:8093/api",
-  backend_user: "http://localhost:8090/api",
-  backend_validation : "http://localhost:8092/api",
-  backend_login : "http://localhost:8091/api",
-  backend_cart : "http://localhost:8094/api",
-  backend_orders : "http://localhost:8095/api",
-  preparation_prod : "deploiement"
-};
