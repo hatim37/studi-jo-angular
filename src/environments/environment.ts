a1183a862d77885e33e97d958e578888cd551c07
unknown <hatim.web.dev@gmail.com>
2025-09-29 22:42:50 +0200

    maj url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   backend_products: process.env['NG_APP_BACKEND_PRODUCTS'] || 'https://studi-jo-products-285073083479.europe-west1.run.app/api',
-  backend_user: process.env['NG_APP_BACKEND_USER'] || 'http://localhost:8090/api',
-  backend_validation: process.env['NG_APP_BACKEND_VALIDATION'] || 'http://localhost:8092/api',
+  backend_user: process.env['NG_APP_BACKEND_USER'] || 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
+  backend_validation: process.env['NG_APP_BACKEND_VALIDATION'] || 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
   backend_login: process.env['NG_APP_BACKEND_LOGIN'] || 'http://localhost:8091/api',
   backend_cart: process.env['NG_APP_BACKEND_CART'] || 'http://localhost:8094/api',
   backend_orders: process.env['NG_APP_BACKEND_ORDERS'] || 'http://localhost:8095/api',
