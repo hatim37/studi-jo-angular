a5a016cd7d1c2a803190387f9674171f071ad9f0
unknown <hatim.web.dev@gmail.com>
2025-09-30 18:20:45 +0200

    maj url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+/*export const environment = {
   backend_products: process.env['NG_APP_BACKEND_PRODUCTS'] || 'https://studi-jo-products-285073083479.europe-west1.run.app/api',
   backend_user: process.env['NG_APP_BACKEND_USER'] || 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
   backend_validation: process.env['NG_APP_BACKEND_VALIDATION'] || 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
@@ -6,4 +6,13 @@ export const environment = {
   backend_cart: process.env['NG_APP_BACKEND_CART'] || 'http://localhost:8094/api',
   backend_orders: process.env['NG_APP_BACKEND_ORDERS'] || 'http://localhost:8095/api',
   preparation_prod: process.env['NG_APP_PREPARATION_PROD'] || 'deploiement'
+};*/
+
+export const environment = {
+  backend_products: 'https://studi-jo-products-285073083479.europe-west1.run.app/api',
+  backend_user: 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
+  backend_validation: 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
+  backend_login: 'http://localhost:8091/api',
+  backend_cart: 'http://localhost:8094/api',
+  backend_orders: 'http://localhost:8095/api',
 };
