27fb13abd1f11068d54f6dc27eeef0152ed02609
unknown <hatim.web.dev@gmail.com>
2025-10-02 17:19:28 +0200

    maj deploiement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,19 @@
-export const environment = {
+/*export const environment = {
   backend_products: process.env['NG_APP_BACKEND_PRODUCTS'],
   backend_user: process.env['NG_APP_BACKEND_USER'],
   backend_validation: process.env['NG_APP_BACKEND_VALIDATION'],
   backend_login: process.env['NG_APP_BACKEND_LOGIN'],
   backend_cart: process.env['NG_APP_BACKEND_CART'],
   backend_orders: process.env['NG_APP_BACKEND_ORDERS'],
+};*/
+
+
+
+export const environment = {
+  backend_products: 'https://studi-jo-products-285073083479.europe-west1.run.app/api',
+  backend_user: 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
+  backend_validation: 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
+  backend_login: 'https://studi-jo-security-285073083479.europe-west1.run.app/api',
+  backend_cart: 'https://studi-jo-cart-285073083479.europe-west1.run.app/api',
+  backend_orders: 'https://studi-jo-orders-285073083479.europe-west1.run.app/api',
 };
-
-
