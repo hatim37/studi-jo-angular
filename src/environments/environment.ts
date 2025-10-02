baa8d94be3fa656797d09de46d0977353f538814
unknown <hatim.web.dev@gmail.com>
2025-10-02 12:07:54 +0200

    maj url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,6 @@ export const environment = {
   backend_user: 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
   backend_validation: 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
   backend_login: 'https://studi-jo-security-285073083479.europe-west1.run.app/api',
-  backend_cart: 'http://localhost:8094/api',
-  backend_orders: 'http://localhost:8095/api',
+  backend_cart: 'https://studi-jo-cart-285073083479.europe-west1.run.app/api',
+  backend_orders: 'https://studi-jo-orders-285073083479.europe-west1.run.app/api',
 };
