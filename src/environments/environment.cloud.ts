8fdb83a4bc5b414cb6142c4944e856cbc58b3461
unknown <hatim.web.dev@gmail.com>
2025-10-08 20:36:23 +0200

    maj

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,20 +1,9 @@
 export const environment = {
-  production: false,
-  backend_products: 'http://localhost:8093/api',
-  backend_user: 'http://localhost:8090/api',
-  backend_validation: 'http://localhost:8092/api',
-  backend_login: 'http://localhost:8091/api',
-  backend_cart: 'http://localhost:8094/api',
-  backend_orders: 'http://localhost:8095/api',
-};
-
-
-
-/*export const environment = {
+  production: true,
   backend_products: 'https://studi-jo-products-285073083479.europe-west1.run.app/api',
   backend_user: 'https://studi-jo-users-285073083479.europe-west1.run.app/api',
   backend_validation: 'https://studi-jo-validation-285073083479.europe-west1.run.app/api',
   backend_login: 'https://studi-jo-security-285073083479.europe-west1.run.app/api',
   backend_cart: 'https://studi-jo-cart-285073083479.europe-west1.run.app/api',
   backend_orders: 'https://studi-jo-orders-285073083479.europe-west1.run.app/api',
-};*/
+};
